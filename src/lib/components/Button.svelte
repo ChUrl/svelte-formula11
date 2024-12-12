0e5795761076c6e13a3ea18e9e3c6b7ace82ff54
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-12 04:36:54 +0100

    Lib: Add button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let {
+    id = "",
+    name = "",
+    formaction = "",
+    label,
+    color,
+    disabled = false,
+  } = $props();
+</script>
+
+<!-- HACK: Set --tw-bg-opacity to 1 so the disabled label/button looks like the disabled input -->
+<button
+  {id}
+  {name}
+  {formaction}
+  {disabled}
+  class="btn btn-{color}"
+  style="--tw-bg-opacity: 1">{label}</button
+>
