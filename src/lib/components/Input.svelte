f46a92d00b73917d40b508f0892ab4a18d7b8b6d
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-12 04:37:11 +0100

    Lib: Add text input

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let {
+    id,
+    name,
+    label,
+    placeholder = "",
+    type = "text",
+    value = "",
+    disabled = false,
+    required = false,
+  } = $props();
+</script>
+
+<label
+  for={id}
+  class="input input-bordered mt-2 flex !cursor-default select-none items-center gap-2"
+>
+  {label}
+  <input
+    {id}
+    {name}
+    {type}
+    class={disabled ? "pointer-events-none grow !cursor-default" : "grow"}
+    {disabled}
+    {required}
+    {placeholder}
+    {value}
+  />
+</label>
