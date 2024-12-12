c0c3e3d792528559fedbb5d4507490c887b7e6c5
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-12 04:37:34 +0100

    Components: Add index.ts for easier importing

---
+++ src/lib/components/index.ts
@@ -0,0 +1,7 @@
+import FileInput from "./FileInput.svelte";
+import Input from "./Input.svelte";
+import Password from "./Password.svelte";
+import Username from "./Username.svelte";
+import Button from "./Button.svelte";
+
+export { FileInput, Input, Username, Password, Button };
