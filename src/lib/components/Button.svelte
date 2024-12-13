5d1ec12d95a8df79e7ca0049fed24708494d7e57
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-13 14:46:02 +0100

    Lib: Allow to manually enable :hover on Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+
+  const is_at_path = (path: string): boolean => {
+    const pathname: string = $page.url.pathname;
+    // console.log(pathname);
+    return pathname === path;
+  };
+
   let {
     children,
-    color,
+    color = undefined,
     submit = false,
     href = undefined,
     fullwidth = false,
+    activate_href = false,
+    activate_button = false,
     ...restProps
   } = $props();
 </script>
 
 {#if href}
-  <a {href} class="btn p-2 variant-filled-{color} {fullwidth ? 'w-full' : 'w-auto'}" {...restProps}>
+  <a
+    {href}
+    class="select-none {color ? `btn px-2 variant-filled-${color}` : ''} {fullwidth
+      ? 'w-full'
+      : 'w-auto'}
+    {activate_href && is_at_path(href) ? 'btn-hover' : ''}"
+    draggable="false"
+    {...restProps}
+  >
     {@render children()}
   </a>
 {:else}
   <button
     type={submit ? "submit" : "button"}
-    class="btn p-2 variant-filled-{color} {fullwidth ? 'w-full' : 'w-auto'}"
+    class="select-none {color ? `btn px-2 variant-filled-${color}` : ''} {fullwidth
+      ? 'w-full'
+      : 'w-auto'}
+    {activate_button ? 'btn-hover' : ''}"
+    draggable="false"
     {...restProps}
   >
     {@render children()}
