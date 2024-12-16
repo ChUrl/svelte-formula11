0d25f23818f732dbe4e92d97e9027a0d524bc36b
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-16 02:27:50 +0100

    Lib: Update index.ts

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,22 +1,35 @@
-import Input from "./Input.svelte";
 import Button from "./Button.svelte";
 import Card from "./Card.svelte";
-import Search from "./Search.svelte";
+import DriverCard from "./DriverCard.svelte";
 import Dropdown from "./Dropdown.svelte";
-// import type DropdownOption from "./Dropdown.svelte";
+import Input from "./Input.svelte";
+import LazyImage from "./LazyImage.svelte";
+import LoadingIndicator from "./LoadingIndicator.svelte";
+import RaceCard from "./RaceCard.svelte";
+import Search from "./Search.svelte";
+import SubstitutionCard from "./SubstitutionCard.svelte";
+import TeamCard from "./TeamCard.svelte";
 
 import MenuDrawerIcon from "./svg/MenuDrawerIcon.svelte";
-import UserIcon from "./svg/UserIcon.svelte";
 import PasswordIcon from "./svg/PasswordIcon.svelte";
+import UserIcon from "./svg/UserIcon.svelte";
 
 export {
-  Input,
+  // Components
   Button,
   Card,
-  Search,
+  DriverCard,
   Dropdown,
-  // type DropdownOption,
+  Input,
+  LazyImage,
+  LoadingIndicator,
+  RaceCard,
+  Search,
+  SubstitutionCard,
+  TeamCard,
+
+  // SVG
   MenuDrawerIcon,
-  UserIcon,
   PasswordIcon,
+  UserIcon,
 };
