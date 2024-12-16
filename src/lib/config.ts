57cae4d400f773d23b2e4a91c658def20bd3e690
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-16 02:27:13 +0100

    Lib: Define some constant values in lib/config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+export const AVATAR_WIDTH: number = 256;
+export const AVATAR_HEIGHT: number = 256;
+
+export const TEAM_LOGO_WIDTH: number = 512;
+export const TEAM_LOGO_HEIGHT: number = 288;
+
+export const DRIVER_HEADSHOT_WIDTH: number = 512;
+export const DRIVER_HEADSHOT_HEIGHT: number = 512;
+
+export const RACE_PICTOGRAM_WIDTH: number = 512;
+export const RACE_PICTOGRAM_HEIGHT: number = 384;
