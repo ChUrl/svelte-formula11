05e32b5ffb5db23e2d9b29eda9aeb5380ba005f9
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-12-18 14:59:55 +0100

    Lib: Move form stuff into form/ directory

--- src/lib/components/form/LazyDropdown.svelte
+++ src/lib/components/form/LazyDropdown.svelte
@@ -4,24 +4,7 @@
   import type { Action } from "svelte/action";
   import type { HTMLInputAttributes } from "svelte/elements";
   import { v4 as uuid } from "uuid";
-  import LazyImage from "./LazyImage.svelte";
-
-  export interface LazyDropdownOption {
-    /** The label displayed in the list of options. */
-    label: string;
-
-    /** The value assigned to the dropdown value variable */
-    value: string;
-
-    /** An optional icon displayed left to the label */
-    icon_url?: string;
-
-    /** The aspect ratio width of the optional icon */
-    icon_width?: number;
-
-    /** The aspect ratio height of the optional icon */
-    icon_height?: number;
-  }
+  import { type LazyDropdownOption, LazyImage } from "$lib/components";
 
   interface LazyDropdownProps extends HTMLInputAttributes {
     children: Snippet;
