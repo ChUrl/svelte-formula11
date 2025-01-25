7ec841101107caf3f5ad89d118471b30a6305f23
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-01-25 17:16:23 +0100

    Data/Users: Sort users by username

--- src/routes/data/users/+layout.server.ts
+++ src/routes/data/users/+layout.server.ts
@@ -3,7 +3,9 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ fetch, locals }) => {
   const fetch_users = async (): Promise<User[]> => {
-    const users: User[] = await locals.pb.collection("users").getFullList({ fetch: fetch });
+    const users: User[] = await locals.pb
+      .collection("users")
+      .getFullList({ fetch: fetch, sort: "+username" });
 
     users.map((user: User) => {
       user.avatar_url = locals.pb.files.getURL(user, user.avatar);
