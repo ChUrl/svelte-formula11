2fb7a7d5973b532c9b69062d1043be698a46637a
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-02-18 00:43:05 +0100

    Seasonpicks: Fetch teams/drivers/seasonpicks

---
+++ src/routes/seasonpicks/+page.ts
@@ -0,0 +1,12 @@
+import { fetch_drivers, fetch_seasonpicks, fetch_teams } from "$lib/fetch";
+import type { PageLoad } from "../$types";
+
+export const load: PageLoad = async ({ fetch, depends }) => {
+  depends("data:teams", "data:drivers", "data:seasonpicks");
+
+  return {
+    teams: fetch_teams(fetch),
+    drivers: fetch_drivers(fetch),
+    seasonpicks: fetch_seasonpicks(fetch),
+  };
+};
