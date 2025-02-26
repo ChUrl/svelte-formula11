08774fc40b9c6ff19e89025536c822bc2f563b76
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-02-26 23:21:43 +0100

    Raceresults: Fetch substitutions

--- src/routes/data/raceresults/+page.ts
+++ src/routes/data/raceresults/+page.ts
@@ -1,4 +1,4 @@
-import { fetch_drivers, fetch_raceresults, fetch_races } from "$lib/fetch";
+import { fetch_drivers, fetch_raceresults, fetch_races, fetch_substitutions } from "$lib/fetch";
 import type { PageLoad } from "../../$types";
 
 export const load: PageLoad = async ({ fetch, depends }) => {
@@ -8,5 +8,6 @@ export const load: PageLoad = async ({ fetch, depends }) => {
     drivers: fetch_drivers(fetch),
     races: fetch_races(fetch),
     raceresults: fetch_raceresults(fetch),
+    substitutions: fetch_substitutions(fetch),
   };
 };
