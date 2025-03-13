965504ed4e596c1ed1a90791d0a7b123901ccd25
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-13 23:59:46 +0100

    Lib: Update date formatting helpers

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,28 @@
 import { format } from "date-fns";
 
-export const format_date = (date: string, formatstring: string): string =>
+/**
+ * 2025-03-28T17:35
+ */
+export const isodatetimeformat: string = "yyyy-MM-dd'T'HH:mm";
+
+/**
+ * 28.03. 17:35
+ */
+export const shortdatetimeformat: string = "dd.MM.' 'HH:mm";
+
+/**
+ * 2025-03-28
+ */
+export const isodateformat: string = "yyyy-MM-dd";
+
+/**
+ * 17:35
+ */
+export const timeformat: string = "HH:mm";
+
+/**
+ * Format a [Date] object using a [date-fns] formatstring.
+ * This function uses localtime instead of UTC.
+ */
+export const format_date = <T extends Date | string>(date: T, formatstring: string): string =>
   format(new Date(date), formatstring);
