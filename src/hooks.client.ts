62f4d211ac860932d30278245dd304a62c9c2c41
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-15 00:19:14 +0100

    Hooks: Try to refresh the authStore on page reload

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,6 +2,12 @@ import { refresh_auth } from "$lib/pocketbase";
 import type { ClientInit } from "@sveltejs/kit";
 
 export const init: ClientInit = async () => {
-  // NOTE: If the auth token is invalidated, this will block the entire page
-  // await refresh_auth();
+  // Try to refresh the authStore. This is required when e.g.
+  // changing e-mail address. The new e-mail will show up after
+  // being verified, so the authStore has to be reloaded.
+  try {
+    await refresh_auth();
+  } catch (error) {
+    console.log("hooks.client.ts:", error);
+  }
 };
