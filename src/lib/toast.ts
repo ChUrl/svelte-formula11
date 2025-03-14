e35d56c81cabd6deb2f25b40b6a2789d01c4990e
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-15 00:19:00 +0100

    Lib: Allow setting timeout in toast helpers

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,28 +1,28 @@
 import type { ToastSettings } from "@skeletonlabs/skeleton";
 
-export const get_info_toast = (message: string): ToastSettings => {
+export const get_info_toast = (message: string, timeout: number = 2000): ToastSettings => {
   return {
     message,
     hideDismiss: true,
-    timeout: 2000,
+    timeout,
     background: "variant-filled-tertiary",
   };
 };
 
-export const get_warning_toast = (message: string): ToastSettings => {
+export const get_warning_toast = (message: string, timeout: number = 2000): ToastSettings => {
   return {
     message,
     hideDismiss: true,
-    timeout: 2000,
+    timeout,
     background: "variant-filled-secondary",
   };
 };
 
-export const get_error_toast = (message: string): ToastSettings => {
+export const get_error_toast = (message: string, timeout: number = 2000): ToastSettings => {
   return {
     message,
     hideDismiss: true,
-    timeout: 2000,
+    timeout,
     background: "variant-filled-primary",
   };
 };
