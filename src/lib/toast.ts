12803a7b8f91041a5f0fc0c9ec3de367eb67ed68
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-14 22:17:54 +0100

    Lib: Add info/warning toast helpers

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,5 +1,23 @@
 import type { ToastSettings } from "@skeletonlabs/skeleton";
 
+export const get_info_toast = (message: string): ToastSettings => {
+  return {
+    message,
+    hideDismiss: true,
+    timeout: 2000,
+    background: "variant-filled-tertiary",
+  };
+};
+
+export const get_warning_toast = (message: string): ToastSettings => {
+  return {
+    message,
+    hideDismiss: true,
+    timeout: 2000,
+    background: "variant-filled-secondary",
+  };
+};
+
 export const get_error_toast = (message: string): ToastSettings => {
   return {
     message,
