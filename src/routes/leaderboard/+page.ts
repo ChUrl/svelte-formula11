7fb38874d9b3f50c15ecc6262dc9497abf2af8ca
Christoph Urlacher <christoph.urlacher@protonmail.com>
2025-03-23 17:16:51 +0100

    Leaderboard: Fetch points data

---
+++ src/routes/leaderboard/+page.ts
@@ -0,0 +1,12 @@
+import { fetch_users, fetch_racepickpoints, fetch_racepickpointsacc } from "$lib/fetch";
+import type { PageLoad } from "../$types";
+
+export const load: PageLoad = async ({ fetch, depends }) => {
+  depends("data:users", "data:raceresults");
+
+  return {
+    users: fetch_users(fetch),
+    racepickpoints: fetch_racepickpoints(fetch),
+    racepickpointsacc: fetch_racepickpointsacc(fetch),
+  };
+};
